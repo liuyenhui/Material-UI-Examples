1fe2995595d0a43bc29872b4c0070b868d64a945
liuyenhui <liuyenhui@gmail.com>
2019-09-03 23:57:23 +0800

    box

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 npm install -g create-react-app
 ### 创建项目 material-ui-examples
 create-react-app material-ui-examples
+### 安装 Material-UI
+yarn add @material-ui/core
+### 安装路由库
+yarn add history react-router react-router-dom
--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Router, Route, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+import BoxE from './Examples/Layout/BoxE.js'
+var hist = createBrowserHistory();
+
+
+ReactDOM.render(<Router history={hist}>
+  <Switch>
+    
+    {/* education */}
+    {/* <Route path="/allcourse" component={AllCourse} /> */}
+    <Route path="/boxe" component={BoxE} />
+    <Route path="/" component={App} />
+
+  </Switch>
+</Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
